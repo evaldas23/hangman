909edb9b8fa588f6e4a0dad0ba67af1e31e4d3a7
evaldas23 <evaldas.cerniauskas@gmail.com>
2020-06-04 09:25:00 +0300

    Starting coding hangman game

---
+++ hangman.js
@@ -0,0 +1,26 @@
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c']
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) ) {
+            puzzle += letter;
+        } else {
+            puzzle += '*'
+        }
+    })
+    
+    return puzzle
+}
+
+
+const game1 = new Hangman('Cat', 2)
+console.log(game1.getPuzzle())
+
+const game2 = new Hangman('New Jersey', 4)
+console.log(game2.getPuzzle())
