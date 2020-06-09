522328fbeba1a783e1bd796dca85e51ebe84562e
evaldas23 <evaldas.cerniauskas@gmail.com>
2020-06-10 02:33:36 +0300

    More flex...

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// Object:  myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+//Funtion: myFunc --> Function.prototype --> Object.prototype --> null
+//String: myString --> String.prototype --> Object.prototype --> null
+//Number: myNumber --> Number.prototype --> Object.prototype --> null
+//Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+
+
+
+const product = 'Computer'
+console.log(product)
+
+const otherProduct = new String('Phone')
+console.log(otherProduct)
--- hangman.js
+++ hangman.js
@@ -1,15 +1,15 @@
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c', 't', 'e']
+    this.guessedLetter = []
 }
 
 Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
 
     this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter;
+        if (this.guessedLetter.includes(letter) || letter === ' ') {
+            puzzle += letter
         } else {
             puzzle += '*'
         }
@@ -18,9 +18,29 @@ Hangman.prototype.getPuzzle = function () {
     return puzzle
 }
 
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    isUnique = !this.guessedLetter.includes(guess)
+    isBadGuess = !this.word.includes(guess)
+
+    if (isUnique) {
+        this.guessedLetter.push(guess)
+    }
+
+    if (isUnique && isBadGuess) {
+        this.remainingGuesses--
+    }
+}
 
 const game1 = new Hangman('Cat', 2)
-console.log(game1.getPuzzle())
 
-const game2 = new Hangman('New Jersey', 4)
-console.log(game2.getPuzzle())
+console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    console.log(game1.getPuzzle())
+    console.log(game1.remainingGuesses)
+})
+ 
