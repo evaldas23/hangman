7ffd6dfa74b2cb78f2d1c425264612a6586ec52c
evaldas23 <evaldas.cerniauskas@gmail.com>
2020-07-07 22:26:36 +0300

    creating HTTP request

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 console.log(game1.status)
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
@@ -15,15 +15,34 @@ window.addEventListener('keypress', function (e) {
 
 })
 
-// Making an http request
- const request = new XMLHttpRequest()
-
- request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error ${error}`)
+    } else {
+        console.log(puzzle)
     }
- })
+  
+})
 
- request.open('GET', 'http://puzzle.mead.io/puzzle')
- request.send()
+// Making an http request
+//(from: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState)
+
+//  //----------------------------------------
+// const countryCode = 'LT'
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((country) => 
+//             country.alpha2Code === countryCode)
+//             console.log(country.name)  
+//     }
+// })
+
+// countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+// countryRequest.send()
+
+
+
+ // 107 paskaita dar perziureti
