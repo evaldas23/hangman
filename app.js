6d28b7d933b1b3dbe696109b5c8b14c066815eb7
evaldas23 <evaldas.cerniauskas@gmail.com>
2020-07-10 23:11:15 +0300

    creating HTTP for world coutrys

--- app.js
+++ app.js
@@ -4,44 +4,41 @@ const game1 = new Hangman('Car parts', 2)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
-console.log(game1.status)
 
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
-    console.log(game1.status)
+  
 
 })
 
-getPuzzle((error, puzzle) => {
+getPuzzle("2", (error, puzzle) => {
     if (error) {
         console.log(`Error ${error}`)
     } else {
         console.log(puzzle)
     }
   
-})
+}) 
+
+//Chalange
+
+getCountry("LT", (error, country) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(`Country name: ${country.name}`)
+
+    }
+} )
 
 // Making an http request
 //(from: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState)
 
 //  //----------------------------------------
-// const countryCode = 'LT'
-// const countryRequest = new XMLHttpRequest()
 
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if(e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const country = data.find((country) => 
-//             country.alpha2Code === countryCode)
-//             console.log(country.name)  
-//     }
-// })
-
-// countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-// countryRequest.send()
 
 
 
--- requests.js
+++ requests.js
@@ -1,16 +1,37 @@
-const getPuzzle = (callback) => {
- const request = new XMLHttpRequest()
+const getPuzzle = (wordCount, callback) => {
+   const request = new XMLHttpRequest()
 
- request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-       const data = JSON.parse(e.target.responseText)
-       callback(undefined, data.puzzle) 
-    } else if  (e.target.readyState === 4) {
-        console.log('An error has taken place', undefined)
-    }
- })
+   request.addEventListener('readystatechange', (e) => {
+      if (e.target.readyState === 4 && e.target.status === 200) {
+         const data = JSON.parse(e.target.responseText)
+         callback(undefined, data.puzzle)
+      } else if (e.target.readyState === 4) {
+         console.log('An error has taken place', undefined)
+      }
+   })
 
- request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
- request.send()
+   request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=3
+ ${wordCount}`)
+   request.send()
 
+}
+
+// Chalange 
+
+const getCountry = (countryCode, callback) => {
+   const countryRequest = new XMLHttpRequest()
+
+   countryRequest.addEventListener('readystatechange', (e) => {
+      if (e.target.readyState === 4 && e.target.status === 200) {
+         const data = JSON.parse(e.target.responseText)
+         const country = data.find((country) =>
+            country.alpha2Code === countryCode)
+         callback(undefined, country)
+      } else if (e.target.readyState === 4) {
+         callback('Unable to fech data')
+      }
+   })
+
+   countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
+   countryRequest.send()
 }
